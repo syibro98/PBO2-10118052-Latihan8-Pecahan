028921830725d60f5a4672d17b25335f2710d67f
Bagus Syibro Malisi <55564197+syibro98@users.noreply.github.com>
2019-09-24 12:45:09 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118052/latihan8/pecahan/PBO210118052Latihan8Pecahan.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118052.latihan8.pecahan;
+
+/**
+ * @author 
+ * NAMA      : Bagus Syibro Malisi
+ * KELAS     : PBO2
+ * NIM       : 10118052
+ * Deskripsi Program : Program ini berisi program untuk menmapilkan 
+ * jenis-jenis bilangan pecahan
+ */
+public class PBO210118052Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float : " + f);
+        System.out.println("Double : " + d);
+    }
+    
+}
